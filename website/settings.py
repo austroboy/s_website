08e2d3d6bbf06008599b564965aa9064cecfde66
Mostasir Mahim <mahimlive@gmail.com>
2026-02-19 21:08:59 +0600

    style (homepage): added hompage components to home.html .

--- website/settings.py
+++ website/settings.py
@@ -137,6 +137,7 @@ CACHES = {
 
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -192,4 +193,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
